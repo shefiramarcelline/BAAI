5ee48821b5c2b7895784aa8047dccf10d5a21c33
Shefira Marcelline <shefira.marcelline@gmail.com>
2024-10-23 22:16:07 +0800

    ok

--- Shefira_g13460766_Simple Tip Calculator.py
+++ Shefira_g13460766_Simple Tip Calculator.py
@@ -6,24 +6,21 @@
 amount = 0
 sum = 0
 total = 0
-i=0
-count = 1
+i = 0
 tip = 0
 
-#1. Input
+# 1. Input
 print("How many people are dining :")
 i = int(input(""))
 
-#2. Process
-while count <= i :
-    print("Enter the amount spent by person",count,":")
+# 2. Process
+for count in range(1, i + 1):
+    print("Enter the amount spent by person", count, ":")
     amount = int(input(""))
     sum = sum + amount
-    count += 1
-
 print("Enter the tip percentage :")
 tip = int(input(""))
-total = sum + sum*tip/100    
+total = sum + sum*tip/100
 
-#3. Output
-print("The total bill including tip is : $",total)
+# 3. Output
+print("The total bill including tip is : $", total)
